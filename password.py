b4a0b8bcbf0a48a838767106f25f877d35389cbd
jennxlee3 <leejen36@msu.edu>
2024-12-18 13:25:29 -0500

    Added implementation of password generator, creating a generate_password function

--- password.py
+++ password.py
@@ -3,4 +3,33 @@ import random
 upp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 low = "abcdefghijklmnopqrstubwxyz"
 sym = "!@#$%^&*()_+-=[]{}|;:,.<>?/~`"
-digits = 
+digits = "0123456789"
+
+
+user_input = input("Would you like a weak or strong password? ")
+
+def generate_password():
+    global password
+    if user_input.lower() == "strong":
+        password = list(''.join([random.choice(set) for set in [upp, low, sym, digits] for _ in range(2)]))
+        random.shuffle(password)
+    elif user_input.lower() == "weak":
+        password = [
+            random.choice(random.choice([upp, low, sym, digits])) for _ in range(4)
+        ]
+        random.shuffle(password)
+    else:
+        print("Invalid input. Type either weak or strong.")
+
+    return ''.join(password)
+
+if __name__ == "__main__":
+    passwrd = generate_password()
+    print("Your new password is:", passwrd)
+    print("Store it somewhere safe!")
+
+
+
+
+
+
